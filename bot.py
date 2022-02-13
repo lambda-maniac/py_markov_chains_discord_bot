380f8b5e9fe4c3767a2f4db1873e94e6ba8c6c23
Sr. Meia-noiteλ (LordZarkares) <lordzarkares@Outlook.com>
2022-02-13 01:15:10 -0300

    the bot itself.

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import secrets
+import json
+
+from markov import *
+
+class Bot (discord.Client):
+    prefix   = "markov"
+    specials = specials = {"-": " ", "@": " ", prefix: "eu"}
+
+    with open("data.json", "r") as file:
+        data = json.loads(''.join(file.readlines()))
+
+        print(f":: Loaded data: {data}")
+
+    async def on_ready(self):
+        print(":: Initializing...")
+
+    async def on_message(self, event):
+        message = event.content.lower()
+
+        if self.prefix in message:
+            await event.channel.send(generate(self.data))
+
+        self.data = learn(tokenize(message, self.specials), self.data)
+
+        with open("data.json", "w+") as file:
+            file.write(json.dumps(self.data, indent = 4))
+
+    async def exit(self):
+        print(":: Closing connection...")
+        await self.close()
+        print(":: Done.")
+
+if __name__ == '__main__' \
+    : Bot().run(secrets.AUTH_TOKEN) ; exit(0)
